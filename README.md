9ef674bd18eaeada99ac892bc146f5eb57b1456c
Marius <marius@example.com>
2022-06-09 11:43:55 +0300

    Route BrowserRouter

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # React router
+
+1. Add router `npm install react-router-dom@^5`
+2. import { BrowserRouter } from 'react-router-dom';
+3. apjuosti musu App su <BrowserRouter></BrowserRouter>
+4. import Route ir aprasyti routus
--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route } from 'react-router-dom';
+
 import './App.css';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
       <h1>Heloo again world</h1>
+      <HomePage />
+      <Route path='/about'>
+        <AboutPage />
+      </Route>
+      <ContactPage />
     </div>
   );
 }
