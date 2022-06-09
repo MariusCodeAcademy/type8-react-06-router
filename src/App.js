3de1fc28b90ed4b5bcaa77506c009676797852e3
Marius <marius@example.com>
2022-06-09 11:49:45 +0300

    a link gives refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route } from 'react-router-dom';
 
 import './App.css';
+import MainNav from './components/MainNav';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -8,12 +9,14 @@ import HomePage from './pages/HomePage';
 function App() {
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
-      <HomePage />
+      <MainNav />
+      {/* <HomePage /> */}
       <Route path='/about'>
         <AboutPage />
       </Route>
-      <ContactPage />
+      <Route path='/contact'>
+        <ContactPage />
+      </Route>
     </div>
   );
 }
---
+++ src/components/MainNav.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const MainNav = () => {
+  return (
+    <nav>
+      <a href='/home'>Home</a>
+      <a href='/about'>About</a>
+      <a href='/contact'>Contact</a>
+    </nav>
+  );
+};
+
+export default MainNav;
