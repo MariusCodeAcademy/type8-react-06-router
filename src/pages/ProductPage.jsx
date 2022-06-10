b664949d94d0128237c19a72c547ab71e28172e1
Marius <marius@example.com>
2022-06-10 10:05:18 +0300

    getProductsAndSetState

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,8 +1,33 @@
-const ProductPage = () => {
-  // state kad issaugoti products array (useState)
+import { useState, useEffect } from 'react';
 
+const ProductPage = () => {
+  console.log('ProductPage ran');
+  // state kad issaugoti products array (useState)
+  const [productsArr, setProductsArr] = useState([]);
   // pasileidzia viena karta uzsikrovus komponentui
-  // useEffect(() => {}, [])
+
+  const getProductsAndSetState = async () => {
+    console.log('getProductsAndSetState ran');
+    const respt = await fetch('/db/products.json');
+    const dataInJs = await respt.json();
+    console.log('dataInJs ===', dataInJs);
+    const onlyDataWeNeed = dataInJs.map((pObj) => {
+      return {
+        id: pObj.id,
+        title: pObj.title,
+        price: pObj.price,
+        image: pObj.image,
+      };
+    });
+    console.log('onlyDataWeNeed  ===', onlyDataWeNeed);
+    setProductsArr(dataInJs);
+  };
+
+  useEffect(() => {
+    console.log('useEffect ran');
+    // pasileidzia viena karta uzsikrovus komponentui
+    getProductsAndSetState();
+  }, []);
 
   return (
     <div>
