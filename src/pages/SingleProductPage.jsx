c032c0cc19b3f947b1561c3706ba9386f9522b2b
Marius <marius@example.com>
2022-06-10 11:47:51 +0300

    getCurrentProductAndSave

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const prId = 5;
+// parissiusti ir issaugoti state produkto kurio id === productId
+// duomenis ir supildyti jsx
+
 const SingleProductPage = () => {
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const { productId } = useParams();
+
+  const getCurrentProductAndSave = async () => {
+    const resp = await fetch('/db/products.json');
+    const dataInJs = await resp.json();
+    console.log('dataInJs ===', dataInJs);
+    console.log('productId ===', typeof productId);
+    const currentProduct = dataInJs.find(({ id }) => id === +productId);
+    console.log('currentProduct ===', currentProduct);
+    setCurrentProduct(currentProduct);
+  };
+
+  useEffect(() => {
+    getCurrentProductAndSave();
+  }, []);
+
   return (
     <div>
-      <h1>Title</h1>
-      <h2>price</h2>
-      <img src='' alt='image' />
-      <p className='description'>descrip</p>
+      <h1>{currentProduct.title}</h1>
+      <h2>{currentProduct.price}</h2>
+      <img src={currentProduct.image} alt='image' />
+      <p className='description'>{currentProduct.description} </p>
+      <button>Go back</button>
     </div>
   );
 };
